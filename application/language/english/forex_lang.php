207ad5649d5c168af052d64a2227c9e84fcb5e99
salmaforex <gundambison@gmail.com>
2015-11-13 19:45:53 +0700

    bahasa

--- application/language/english/forex_lang.php
+++ application/language/english/forex_lang.php
@@ -2,12 +2,24 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 $lang['forex_firstname'] = 'Fist Name';
+$lang['forex_fullname'] = 'Full Name';
 $lang['forex_lastname'] = 'Last Name';
 $lang['forex_inputsuggestion'] = 'Please input carefully';
+$lang['forex_inputsuggestion2'] = 'Please input more than 2 words';
+$lang['forex_inputpassword'] = 'Please use safety password';
 $lang['forex_dob']='Date of Birth';
-$lang['forex_country']='Citizenship';
+$lang['forex_country']='Country';
+
+$lang['forex_state']='State';
+$lang['forex_city']='City';
+$lang['forex_zipcode']='Zip code';
+$lang['forex_address']='Address';
 
 $lang['forex_email']='Email';
 $lang['forex_phone']='Phone';
+
+$lang['forex_pass']='Access Password';
+$lang['forex_passconfirm']='Confirm Access Password';
+$lang['forex_agent']='Agent';
 
 ?>
