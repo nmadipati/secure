965d0a1445c460ddbd9321355a5cfd62dd9e9cc7
salmaforex <gundambison@gmail.com>
2015-11-13 12:58:36 +0700

    update style dan js utama

--- assets/js/forex.js
+++ assets/js/forex.js
@@ -1,8 +1,17 @@
+function clearModal(){
+	jQuery(".modal-title, .modal-body").empty();
+}
 /* FOREX */
 function createLiveUser(){
 	var url=siteUrl+"forex/data";
 	var formData=jQuery("#frmLiveAccount").serializeArray();
 	params={type:"request",data:formData}
+	stat=checkInput();
+	if(stat==0){
+		alert('please check your input');
+		return false;
+	}
+	
 	respon=sendAjax(url,params);
 	respon.success(function(result,status) {
 		  clearModal();
@@ -15,10 +24,9 @@ function createLiveUser(){
 			
 		}
 		
-		jQuery("#myModal").modal({show: true});	
-		console.log("success");
-			console.log(result);
-			
+		jQuery("#myModal").modal({show: true}).css("height","150%");	
+		
+		console.log("success");	console.log(result);			
 	   });
 	   respon.error(function(xhr,status,msg){			
 			console.log("Error");
@@ -29,13 +37,58 @@ function createLiveUser(){
 		});
 }
 
+function checkInput(){
+	stat=1
+	stat=checkMoreThan(jQuery('#input_name'),2);
+	if(stat==1)stat=checkMoreThan(jQuery('#input_address'),2);
+	if(stat==1)stat=checkMoreThan(jQuery('#input_state'),2);
+	if(stat==1)stat=checkMoreThan(jQuery('#input_city'),2);
+	if(stat==1)stat=checkMoreThan(jQuery('#input_zipcode'),2);
+	//if(stat==1)stat=checkMoreThan(jQuery('#input_agent'),2);
+	if(stat==1){
+		stat=checkEmail(jQuery('#input_email'),2);
+		if(stat==false){
+			jQuery('#input_email').css('border-color','#ff2323') ;
+			jQuery('#input_email').css('border-width','3px') ;
+		}
+	}
+	if(stat==1||stat==true){		
+		jQuery('#input_email').css('border-color','#e1e1e1') ;
+		jQuery('#input_email').css('border-width','1px') ;
+		stat=checkMoreThan(jQuery('#input_phone'),2);
+	}else{ 
+		console.log('error email :');console.log(stat);
+		
+	}
+	return stat;
+}
+
+function checkEmail(target){
+    var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+    return re.test(target.val());
+	
+}
+function checkMoreThan(target, length){
+	if(target.val().length<=length){
+		console.log('error :'+target.attr('name'));
+		console.log('error :'+target.val().length);
+		target.css('border-color','#ff2323') ;
+		target.css('border-width','3px') ;
+		return 0;
+	}
+	target.css('border-color','#E1E1E1') ;
+		target.css('border-width','1px') ;
+	return 1;
+}
 
 jQuery(function() {
     jQuery( ".datepicker,#input_dob" ).datepicker({
 	dateFormat:'yy-mm-dd',
 	showAnim: "fold"
 	});
-	
+	jQuery("#myModal").click(function(){
+		jQuery("#myModal").hide();
+	});
  
   });
   
@@ -54,4 +107,5 @@ function sendAjax(url,params){
 	return request;
 }
 
+jQuery("#myModal").modal({show: false});	
 //jQuery( ".datepicker, #input_dob" ).datepicker({ dateFormat:'yy-mm-dd'});
